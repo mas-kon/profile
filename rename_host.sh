947b5c9571368139a2599b0699e95bc9ba8be69c
Konstantin <31608451+mas-kon@users.noreply.github.com>
2021-06-19 14:40:21 +0300

    Update rename_host.sh

--- rename_host.sh
+++ rename_host.sh
@@ -4,10 +4,10 @@ if [ -n "$1" ]
 
 then
     hostnamectl set-hostname $1
-    sed -i 's/TEMPLATE/$1/' /etc/hosts
-    sed -i 's/TEMPLATE/$1/' /etc/hostname
-    sed -i 's/TEMPLATE/$1/' /etc/mailname
-    sed -i 's/TEMPLATE/$1/' /etc/postfix/main.cf
+    sed -i 's/TEMPLATE/$1/g' /etc/hosts
+    sed -i 's/TEMPLATE/$1/g' /etc/hostname
+    sed -i 's/TEMPLATE/$1/g' /etc/mailname
+    sed -i 's/TEMPLATE/$1/g' /etc/postfix/main.cf
 else
     echo "No parameters found. "
 fi
