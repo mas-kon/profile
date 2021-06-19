21660a4bfc63377a6119a617a79c176bad299b29
Konstantin <31608451+mas-kon@users.noreply.github.com>
2021-06-19 14:36:46 +0300

    Update rename_host.sh

--- rename_host.sh
+++ rename_host.sh
@@ -5,7 +5,7 @@ if [ -n "$1" ]
 then
     hostnamectl set-hostname $1
     sed -i 's/TEMPLATE/$1/' /etc/hosts
-    sed -i 's/TEMPLATE/$1/' /etc/hostsname
+    sed -i 's/TEMPLATE/$1/' /etc/hostname
     sed -i 's/TEMPLATE/$1/' /etc/mailname
     sed -i 's/TEMPLATE/$1/' /etc/postfix/main.cf
 else
