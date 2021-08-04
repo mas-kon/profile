eb80f9a137d9ceef1fe60b434e22bb7525dbc56f
Konstantin <31608451+mas-kon@users.noreply.github.com>
2021-08-04 22:02:41 +0300

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -18,8 +18,8 @@ echo "alias m='more'" >> .bashrc
 echo "alias g='grep'" >> .bashrc
 echo "alias tt='tail -f'" >> .bashrc
 echo "alias ll='ls -lha --color=auto'" >> .bashrc
-echo "    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> .bashrc
+echo "    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '" >> .bashrc
 echo "else" >> .bashrc
-echo "    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\# '" >> .bashrc
+echo "    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]# '" >> .bashrc
 echo "fi" >> .bashrc
 source .bashrc
