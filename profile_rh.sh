20b576d1263892777ee363f39945743d39518c86
Konstantin <31608451+mas-kon@users.noreply.github.com>
2022-11-05 21:30:04 +0200

    Update profile_rh.sh

--- profile_rh.sh
+++ profile_rh.sh
@@ -3,6 +3,9 @@
 yum -y update
 yum -y install wget vim mc net-tools dnsutils mlocate htop git ntpdate iotop tmux gpg bash-completion
 
+sudo -u ec2-user
+cd /home/ec2-user
+
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
@@ -22,9 +25,6 @@ echo "alias g='grep --colour=always'" >> .bashrc
 echo "alias tt='tail -f'" >> .bashrc
 echo "alias getip='wget -qO- eth0.me'" >> .bashrc
 echo "alias ll='ls -lha --color=auto'" >> .bashrc
-echo "if [ "$(whoami)" != 'root' ]" >> .bashrc
-echo " then" >> .bashrc
-echo "    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '" >> .bashrc
-echo "else" >> .bashrc
-echo "    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]# '" >> .bashrc
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '" >> .bashrc
 echo "fi" >> .bashrc
+exit
