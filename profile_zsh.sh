32758a6698846ec8c133e8e0e7442040e988173a
Konstantin <31608451+mas-kon@users.noreply.github.com>
2023-10-07 14:33:00 +0300

    Update profile_zsh.sh

--- profile_zsh.sh
+++ profile_zsh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install zsh bat curl vim mc net-tools dnsutils mlocate htop git ntpdate iotop tmux gpg parted bash-completion fonts-powerline -y
+# Setup 
+sudo apt update && sudo apt install zsh bat curl vim mc net-tools dnsutils mlocate htop git chrony iotop tmux gpg parted bash-completion fonts-powerline ca-certificates apt-transport-https -y
 
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
