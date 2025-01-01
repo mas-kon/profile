17f6608715fe1467e3163b562b1392642696f284
Konstantin <31608451+mas-kon@users.noreply.github.com>
2025-01-01 18:48:48 +0200

    Update profile_zsh.sh

--- profile_zsh.sh
+++ profile_zsh.sh
@@ -15,6 +15,11 @@ sudo apt update && sudo apt install -y chrony fzf ripgrep xclip gdu zsh bat eza 
 
 sudo apt autoremove
 
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
