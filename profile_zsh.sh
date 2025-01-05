75d230bd96ef26e4c0d3162c772ed448e17eef05
Konstantin <31608451+mas-kon@users.noreply.github.com>
2025-01-05 07:58:16 +0200

    Update profile_zsh.sh

--- profile_zsh.sh
+++ profile_zsh.sh
@@ -9,13 +9,15 @@ if [[ ! -f /etc/sudoers.d/${USER} || "$UID" -ne 0 ]]; then
 fi
 
 # Install package
-sudo apt update && sudo apt install -y chrony fzf ripgrep xclip gdu zsh bat curl vim mc tree net-tools bash-completion \
-            dnsutils htop git chrony iotop tmux gpg parted fonts-powerline ca-certificates apt-transport-https sysstat ncdu \
-            build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt update && sudo apt install -y tree mc bat zsh chrony curl vim htop git build-essential ca-certificates apt-transport-https sysstat ncdu
 
 set +e
+sudo apt install -y fzf ripgrep gdu net-tools bash-completion duf dnsutils iotop tmux gpg parted fonts-powerline fd-find \
+            libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 sudo apt install -y eza
 sudo apt install -y exa 
+sudo apt install -y gping
+sudo apt install -y duf
 sudo systemctl restart systemd-timesyncd
 
 set -e
@@ -126,6 +128,7 @@ rm -rf /tmp/nvim
     echo "alias less='less -F'"
     echo "alias q='exit'"
     echo "alias m='more'"
+    echo "alias du='duf'"
     echo "alias grep='grep --colour=always'"
     echo "alias g='grep --colour=always'"
     echo "alias tt='tail -f'"
